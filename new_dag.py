dbb3efb0efcb6e183f584c1ab15988995b09aa30
Theo Bouwman <theobouwman@icloud.com>
2024-12-19 02:52:10 +0100

    Delete new_dag.py

--- new_dag.py
+++
@@ -1,7 +0,0 @@
-dag = DAG('hello_world', description='Hello World DAG',
-          schedule_interval='0 12 * * *',
-          start_date=datetime(2022, 8, 24), catchup=False)
-
-hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
-
-hello_operator
